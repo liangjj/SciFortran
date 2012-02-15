46db6f816db74b8e4ba09b79979e4e27df4c55c0
Adriano Amaricci <adriano.amaricci@gmail.com>
2012-02-15 12:56:27 +0100

    Useless update w/ respect to previous

--- numutils/numstat.f90
+++ numutils/numstat.f90
@@ -14,6 +14,7 @@ program numstat
   logical                         :: get_skew=.false.
   logical                         :: get_kurt=.false.
   logical                         :: get_min=.false.,get_max=.false.
+  logical                         :: get_minloc=.false.,get_maxloc=.false.
   logical                         :: get_sum=.false.
   logical                         :: get_cov=.false.,get_corr=.false.
   real(8),allocatable             :: data_mean(:)
@@ -22,6 +23,7 @@ program numstat
   real(8),allocatable             :: data_skew(:)
   real(8),allocatable             :: data_kurt(:)
   real(8),allocatable             :: data_min(:),data_max(:)
+  integer,allocatable             :: data_minloc(:),data_maxloc(:)
   real(8),allocatable             :: data_sum(:)
   real(8),allocatable             :: data_covariance(:,:)
   !add more here:
@@ -32,7 +34,7 @@ program numstat
        '  numstat',&
        '',&
        'SYNOPSIS',&
-       '  numstat [n/ncol] [all/mean/sdev/min/max/skew/kurt/sum/covar/corr]',&
+       '  numstat [n/ncol] [all/mean/sdev/skew/kurt/min/max/minloc/maxloc/sum/covar/corr]',&
        '',&
        'DESCRIPTION',&
        '  Read an array *X(:) or *Y(:,:) from stdin and return statistical description of this sample',&
@@ -43,6 +45,8 @@ program numstat
        ' sdev      -- standard deviation of the sample. 2nd moment.',&
        ' min       -- min of the sample.',&
        ' max       -- max of the sample.',&
+       ' minloc    -- min location of the sample.',&
+       ' maxloc    -- max location of the sample.',&
        ' skew      -- skewness of the sample. 3rd moment.',&
        ' kurt      -- kurtosis of the sample. 4th moment.',&
        ' sum       -- sum of the sample.',&
@@ -61,6 +65,8 @@ program numstat
      if(cmd_var%value=="kurt") get_kurt=.true.
      if(cmd_var%value=="min")  get_min=.true.
      if(cmd_var%value=="max")  get_max=.true.
+     if(cmd_var%value=="minloc")get_minloc=.true.
+     if(cmd_var%value=="maxloc")get_maxloc=.true.
      if(cmd_var%value=="sum")  get_sum=.true.
      if(cmd_var%value=="covar")get_cov=.true.
      if(cmd_var%value=="corr") get_corr=.true.
@@ -75,6 +81,8 @@ program numstat
      get_kurt=.true.
      get_min=.true.
      get_max=.true.
+     get_minloc=.true.
+     get_maxloc=.true.
      get_sum=.true.
      get_cov=.true.
      get_corr=.true.
@@ -106,17 +114,20 @@ program numstat
   allocate(data_var(ncol))
   allocate(data_skew(ncol))
   allocate(data_kurt(ncol))
-  allocate(data_min(ncol))
-  allocate(data_max(ncol))
+  allocate(data_min(ncol),data_minloc(ncol))
+  allocate(data_max(ncol),data_maxloc(ncol))
   allocate(data_sum(ncol))
   allocate(data_covariance(ncol,ncol))
+
+
   do icol=1,ncol
      call get_moments(data(icol,:),data_mean(icol),data_sdev(icol),data_var(icol),data_skew(icol),data_kurt(icol))
      data_min(icol)=minval(data(icol,:))
      data_max(icol)=maxval(data(icol,:))
+     data_minloc(icol)=minloc(data(icol,:),dim=1)
+     data_maxloc(icol)=maxloc(data(icol,:),dim=1)
      data_sum(icol)=sum(data(icol,:))
   enddo
-
 
   if(get_mean)write(*,"(A8,100f24.12)")"mean:",(data_mean(i),i=1,ncol)
   if(get_sdev)write(*,"(A8,100f24.12)")"std.dev:",(data_sdev(i),i=1,ncol)
@@ -125,6 +136,8 @@ program numstat
   if(get_kurt)write(*,"(A8,100f24.12)")"kurt:",(data_kurt(i),i=1,ncol)
   if(get_min)write(*,"(A8,100f24.12)")"min:",(data_min(i),i=1,ncol)
   if(get_max)write(*,"(A8,100f24.12)")"max:",(data_max(i),i=1,ncol)
+  if(get_minloc)write(*,"(A8,100I12)")"minloc:",(data_minloc(i),i=1,ncol)
+  if(get_maxloc)write(*,"(A8,100I12)")"maxloc:",(data_maxloc(i),i=1,ncol)
   if(get_sum)write(*,"(A8,100f24.12)")"sum:",(data_sum(i),i=1,ncol)
   if(ncol>1)then
      data_covariance = get_covariance(data,data_mean)

5e52cc3043f391aa192ab7b32de4b2dee00cb064
Adriano Amaricci <adriano.amaricci@gmail.com>
2012-09-07 12:15:19 +0200

    Update. New version for splot_3d using write statment, renamed TIMER library and few other minor updates

--- numutils/src/deriv.f90
+++ numutils/src/deriv.f90
@@ -31,7 +31,7 @@ program deriv_
        '  dh=[0.0] -- infinitesimal increment.',&
        '  '])
 
-  if(command_argument_count()==0)call print_cmd_help(help_buffer)
+  !if(command_argument_count()==0)call print_cmd_help(help_buffer)
   call parse_cmd_help(help_buffer)
   call parse_cmd_variable(dh,"DH",default=0.d0)
   !

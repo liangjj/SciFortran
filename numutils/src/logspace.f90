6e77aabd27139ebe00e56b580097f5a475573d73
Adriano Amaricci <adriano.amaricci@gmail.com>
2013-05-10 14:49:15 +0200

    update: fixed bug in tools_grids/logspace.

---
+++ numutils/src/logspace.f90
@@ -0,0 +1,51 @@
+program logsp
+  USE COMMON_VARS
+  USE PARSE_CMD
+  USE TOOLS
+  implicit none
+  integer             :: i,L,pos1,pos2
+  real(8)             :: wmin,wmax
+  real(8),allocatable :: array(:)
+  character(len=256),allocatable :: help_buffer(:)
+
+  allocate(help_buffer(15))
+  help_buffer=([&
+       'NAME',&
+       '  logspace',&
+       '',&
+       'SYNOPSIS',&
+       '  logspace [wmin/a] [wmax/b] [L]',&
+       '',&
+       'DESCRIPTION',&
+       '  Returns evenly spaced numbers over a specified interval',&
+       ' ',&
+       'OPTIONS',&
+       ' wmin/a=[1.d-12]   -- starting value of the sequence',&
+       ' wmax/b=[1]    -- the end point of the sequence ',&
+       ' L=[1000]      -- length of the sequence',&
+       ' range=[a:b:L] -- pass the whole range of the sequence at once',&
+       '  '])
+
+  call parse_cmd_help(help_buffer)
+
+  call parse_cmd_variable(L,"L",default=1000)
+  call parse_cmd_variable(wmin,"WMIN","A",default=0.d0)
+  call parse_cmd_variable(wmax,"WMAX","B",default=1.d0)
+  do i=1,command_argument_count()
+     nml_var=get_cmd_variable(i)
+     if(nml_var%name=="RANGE")then
+        pos1=scan(nml_var%value,":")
+        pos2=scan(nml_var%value(pos1+1:),":")
+        if(nml_var%value(1:pos1-1)/="")read(nml_var%value(1:pos1-1),*)wmin
+        if(nml_var%value(pos1+1:pos1+pos2-1)/="")read(nml_var%value(pos1+1:pos1+pos2-1),*)wmax
+        if(nml_var%value(pos1+pos2+1:)/="")read(nml_var%value(pos1+pos2+1:),*)L
+     endif
+  enddo
+
+  allocate(array(L))
+  array = logspace(wmin,wmax,L)
+  do i=1,L
+     write(*,*)array(i)
+  enddo
+  deallocate(array)
+end program logsp
